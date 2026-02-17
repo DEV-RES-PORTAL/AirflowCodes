17fb10f8c54a201cfdd719dbd2a3d71b0027d1ab
DEV-RES-PORTAL <dev_portal@res-e.ru>
2026-02-17 17:19:54 +0300

    Update check_dbt.py

---
+++ codes/check_dbt.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+from airflow.decorators import task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.bash import BashOperator
+from airflow import DAG
+
+
+CRON_EXP = None
+START_DATE = datetime(2025, 11, 27, 4, 0, 0, 0)
+
+default_args = {
+    "owner": "DEV",
+    "retries": 0
+}
+
+with DAG(
+    dag_id="check_dbt",
+    default_args=default_args,
+    start_date=START_DATE,
+    schedule_interval=CRON_EXP,
+    catchup=False
+) as dags:
+    
+    dbt_run = BashOperator(
+        task_id="dbt_run",
+        bash_command="cd dbt_project && dbt run -m my_first_dbt_model",
+        env={
+            "DBT_PROFILES_DIR": "dbt_project"
+        }
+    )
